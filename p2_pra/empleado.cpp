7e870ff6d63039857ec76ecf11d9211268472f5c
Ronald Barreto <128637564+Ronaldbar@users.noreply.github.com>
2024-10-31 11:27:53 -0500

    Update empleado.cpp

--- p2_pra/empleado.cpp
+++ p2_pra/empleado.cpp
@@ -1,14 +1,87 @@
 #include "empleado.h"
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
 
 // Constructor
-Empleado::Empleado(const std::string& nombre, const std::string& apellido, 
-    const unsigned int& cedula, double salarioMensual)
-    : _nombre(nombre), _apellido(apellido), _cedula(cedula), 
-    _salarioMensual(salarioMensual) 
+Empleado::Empleado()
     {
+        _nombre = "";
+        _apellido = "";
+        _cedula = "";
+        _salario = 0.0;
         _horasExDia.clear();
     }
 
+float Empleado::salario() const
+{
+    return _salario;
+}
 
+void Empleado::setSalario(float salario)
+{
+    _salario = salario;
+}
+
+string Empleado::cedula() const
+{
+    return _cedula;
+}
+
+void Empleado::setCedula(const string &cedula)
+{
+    _cedula = cedula;
+}
+
+std::string Empleado::apellido() const
+{
+    return _apellido;
+}
+
+void Empleado::setApellido(const string &apellido)
+{
+    _apellido = apellido;
+}
+
+string Empleado::nombre() const
+{
+    return _nombre;
+}
+
+void Empleado::setNombre(const string &nombre)
+{
+    _nombre = nombre;
+}
+
+void Empleado::agregarHorasExtras(int dia, float horas) {
+    HorasExtras a(dia, horas);
+    _horasExDia.push_back(a);
+    return;
+}
+
+bool Empleado::ordenar() {
+    _horasExDia.sort();
+    _horasExDia.reverse();
+    return true;
+}
+
+float Empleado::totalHorasExtras() const {
+    float total = 0;
+    for (list<HorasExtras>::const_iterator it = _horasExDia.begin(); it != _horasExDia.end(); ++it) {
+        total += it->second;
+    }
+    return total;
+}
+
+float Empleado::calcularHE(float salario, float totalHorasExtras) {
+    float valorHE = 0;
+    valorHE = (salario / 240) * totalHorasExtras;
+    return valorHE;
+}
+
+bool Empleado::compararHE(const Empleado &a, const Empleado &b) {
+    return a.totalHorasExtras() < b.totalHorasExtras();
+}
