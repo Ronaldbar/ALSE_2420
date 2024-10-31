f97d51f38d98161c186c5e90edfde2158ef5297a
Ronald Barreto <128637564+Ronaldbar@users.noreply.github.com>
2024-10-31 11:28:14 -0500

    Update empleado.h

--- p2_pra/empleado.h
+++ p2_pra/empleado.h
@@ -3,20 +3,46 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <list>
 
 typedef std::pair<int, float> HorasExtras; // (día, horas)
+
 class Empleado {
     std::string _nombre;
-    
-
-    std::vector<HorasExtras> _horasExDia;
+    std::string _apellido;
+    std::string _cedula;
+    float _salario;
+    std::list<HorasExtras> _horasExDia;
 
 public:
     Empleado(const std::string& nombre, const std::string& apellido, 
-        const unsigned int& cedula, double salarioMensual);
+        const unsigned int& cedula, float salario);
+
+    Empleado();
+
+    std::string nombre() const;
+    void setNombre(const string &nombre);
+
+    string apellido() const;
+    void setApellido(const std::string &apellido);
+
+    std::string cedula() const;
+    void setCedula(const std::string &cedula);
+
+    float salario() const;
+    void setSalario(float salario);
 
     void agregarHorasExtras(int dia, float horas) ;
 
+    float totalHorasExtras() const;
+
+    bool ordenar();
+
+    float calcularHE(float salario, float totalHorasExtras);
+
+    static bool compararHE(const Empleado& a, const Empleado& b);
+
 };
 
-#endif // EMPLEADO_H
+#endif
